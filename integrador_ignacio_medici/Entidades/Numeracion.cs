2614327153d7deb3f8d833933485cffe921c14ef
Ignacio Medici <88944206+Ignacio630@users.noreply.github.com>
2023-10-01 21:26:21 -0300

    Add files via upload
    Falta binario

---
+++ integrador_ignacio_medici/Entidades/Numeracion.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numeracion
+    {
+        private ESistema sistema;
+        private double valorNumerico;
+        private double valorDecimal;
+
+        public ESistema Sistema 
+        {
+            get 
+            {
+                return this.sistema;
+            }
+        }
+        public string ValorNumerico
+        {
+            get
+            {
+                return this.valorNumerico.ToString();
+            }
+        }
+
+        public double BinarioADecimal(string valor)
+        {
+            double valorDecimal = Convert.ToInt32(valor, 2);
+            if (EsBinario(valor))
+            {
+                return 0;
+            }
+            return valorDecimal;
+        }
+
+        
+        public string ConvertirA(ESistema sistema)
+        {
+
+            string retorno = string.Empty;
+
+            if (this.Sistema == sistema)
+            {
+                retorno = this.ValorNumerico;
+            }
+            else
+            {
+                if (ESistema.Decimal == sistema)
+                {
+                    retorno = this.BinarioADecimal(this.ValorNumerico).ToString();
+                }
+                else 
+                {
+                    retorno = this.DecimalABinario(this.ValorNumerico);
+                }
+            }
+            return retorno;
+        }
+
+
+
+        private void InicializarValores(string valor, ESistema sistema)
+        {
+            if (EsBinario(valor))
+            {
+                double.TryParse(this.ConvertirA(sistema),out double valorParsed);
+                this.valorNumerico = valorParsed;
+                this.sistema = sistema;
+            }
+            else if (sistema == ESistema.Decimal)
+            {
+                double.TryParse(valor, out double valorParsed);
+                this.valorNumerico = valorParsed;
+                this.sistema = sistema;
+            }
+            else
+            {
+                this.valorNumerico = double.MinValue;
+            }
+
+        }
+
+        private string DecimalABinario(int valor)
+        {
+            string mensajeError = "Numero invalido";
+            string valorBinario = Convert.ToString(valor,2);
+
+            if (valor < 0)
+            {
+                return mensajeError;
+            }
+
+            return valorBinario;
+        }
+
+        private string DecimalABinario(string valor)
+        {
+
+            if(int.TryParse(valor, out int valorDecimal))
+            {
+                return this.DecimalABinario(valorDecimal);
+            }
+            else
+            {
+                return "Numero invalido";
+            }
+        }
+
+        public bool EsBinario(string valor)
+        {
+            foreach(char caracter in valor)
+            {
+                if(caracter != '0' && caracter != '1')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public Numeracion(ESistema sistema, double valorNumerico) 
+        { 
+            this.sistema = sistema;
+            this.valorNumerico = valorNumerico;
+        }
+        public Numeracion(string valor, ESistema sistema)
+        { 
+            this.InicializarValores(valor, sistema);
+        }
+        public static bool operator ==(ESistema sistema, Numeracion numeracion)
+        {
+            return sistema == numeracion.sistema;
+        }
+        public static bool operator ==(Numeracion n1, Numeracion n2)
+        {
+            return n1.sistema == n2.sistema;
+        }
+        public static bool operator!=(ESistema sistema, Numeracion numeracion)
+        {
+            return sistema != numeracion.sistema;
+        }
+        public static bool operator!=(Numeracion n1,Numeracion n2)
+        {
+            return n1.sistema != n2.sistema;
+        }
+        public static Numeracion operator+(Numeracion n1,Numeracion n2)
+        {
+            double aux1, aux2;
+
+            double.TryParse(n1.ValorNumerico, out aux1);
+            double.TryParse(n2.ValorNumerico, out aux2);
+
+            return new Numeracion(ESistema.Decimal, aux1 + aux2);
+        }
+        public static Numeracion operator-(Numeracion n1, Numeracion n2)
+        {
+            double aux1, aux2;
+
+            double.TryParse(n1.ValorNumerico, out aux1);
+            double.TryParse(n2.ValorNumerico, out aux2);
+
+            return new Numeracion(ESistema.Decimal, aux1 - aux2);
+        }
+        public static Numeracion operator*(Numeracion n1, Numeracion n2)
+        {
+            double aux1, aux2;
+
+            double.TryParse(n1.ValorNumerico, out aux1);
+            double.TryParse(n2.ValorNumerico, out aux2);
+
+            return new Numeracion(ESistema.Decimal, aux1 * aux2);
+        }
+        public static Numeracion operator/(Numeracion n1, Numeracion n2)
+        {
+            double aux1, aux2;
+
+            double.TryParse(n1.ValorNumerico, out aux1);
+            double.TryParse(n2.ValorNumerico, out aux2);
+
+            return new Numeracion(ESistema.Decimal, aux1 / aux2);
+        }
+
+    }
+
+    public enum ESistema
+    {
+        Decimal = 10,
+        Binario 
+    }
+}
---
+++ integrador_ignacio_medici/Entidades/Operacion.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Entidades
+{
+    public class Operacion
+    {
+        private Numeracion primerOperando;
+        private Numeracion segundoOperando;
+
+        private Numeracion PrimerOperando
+        {
+            get
+            {
+                return this.primerOperando;
+            }
+            set
+            {
+                this.primerOperando = value;
+            }
+        }
+        private Numeracion SegundoOperando
+        {
+            get
+            {
+                return this.segundoOperando;
+            }
+            set
+            {
+                this.segundoOperando = value;
+            }
+        }
+        public Operacion(Numeracion primerOperando, Numeracion segundoOperando)
+        {
+            this.primerOperando = primerOperando;
+            this.segundoOperando = segundoOperando;
+        }
+
+        public Numeracion Operar(char operador)
+        {
+            Numeracion resultadoOperacion;
+           
+
+            switch (operador)
+            {
+                case '+':
+                    
+                    resultadoOperacion = this.primerOperando + this.segundoOperando;
+
+                    break;
+                case '-':
+
+                    resultadoOperacion = this.primerOperando - this.segundoOperando;
+
+                    break;
+                case '/':
+
+                    if(this.segundoOperando.ValorNumerico != "0")
+                    {
+                        resultadoOperacion = this.primerOperando / this.segundoOperando;
+                    }
+                    else
+                    {
+                        resultadoOperacion = this.primerOperando;
+                    }
+                    break;
+
+                case '*':
+
+                    resultadoOperacion = this.primerOperando * this.segundoOperando;
+
+                    break;
+
+                default:
+
+                    resultadoOperacion = this.primerOperando;
+                    
+                    break;
+            }
+            return resultadoOperacion;
+        }
+
+
+
+
+
+        
+        
+        
+    }
+
+
+}
